34c51d0b2ef1b5597612762a8e9b0da0d4b4cc21
aaronchu415 <aaronchu415@gmail.com>
2019-03-29 16:04:22 -0700

    coverted bot to oop

--- connect4.js
+++ connect4.js
@@ -19,10 +19,71 @@ class Game {
     this.currPlayer = players[0];
     this.freezeBoard = false;
 
+    this.bot = new Bot(this)
+
     this.players = players
 
     this.makeBoard();
     this.makeHtmlBoard();
+
+    this.count = 0
+
+    this.doBotMove()
+  }
+
+  doBotMove() {
+
+    if (this.bot === null) return
+
+    let move = 0
+    let moves = []
+
+    if (this.currPlayer.id === 1) {
+
+      let winningMoves = this.bot.findWinningMove(this.board, 1)
+      let defenseMoves = this.bot.findDefenceMove(this.board, 1)
+      let futureWinMove = this.bot.findFutureWinningMove(this.board, 1)
+      futureWinMove = this.bot.checkIfMoveWillResultInEnemyWinning(futureWinMove, this.board)
+      futureWinMove = this.bot.checkIfMoveWillResultInPincerAttack(futureWinMove, this.board)
+      let futureDefenseMove = this.bot.findFutureDefenseMove(this.board, 1)
+      futureDefenseMove = this.bot.checkIfMoveWillResultInEnemyWinning(futureDefenseMove, this.board)
+      futureDefenseMove = this.bot.checkIfMoveWillResultInPincerAttack(futureDefenseMove, this.board)
+      let futurePincerMove = this.bot.findFuturePincerMove(this.board, 1)
+      futurePincerMove = this.bot.checkIfMoveWillResultInEnemyWinning(futurePincerMove, this.board)
+      let pincerDefenseMove = this.bot.findPincerDefenseMove(this.board, 1)
+      pincerDefenseMove = this.bot.checkIfMoveWillResultInEnemyWinning(pincerDefenseMove, this.board)
+      pincerDefenseMove = this.bot.checkIfMoveWillResultInPincerAttack(pincerDefenseMove, this.board)
+
+      moves = winningMoves.concat(defenseMoves).concat(futurePincerMove).concat(pincerDefenseMove).concat(futureWinMove).concat(futureDefenseMove)
+      console.log("winning move is " + winningMoves)
+      console.log("defense move is " + defenseMoves)
+      console.log("pincer move is " + futurePincerMove)
+      console.log("pincer defence move is " + pincerDefenseMove)
+      console.log("future winning move is " + futureWinMove)
+      console.log("future defense move is " + futureDefenseMove)
+      console.log('---------------------')
+
+      if (moves.length === 0) {
+        move = Math.floor(Math.random() * (this.width - 1))
+        var y = this.bot.findSpotForCol(this.board, move, this.board.length);
+        if (y !== null) {
+
+          if ((this.bot.checkIfMoveWillResultInEnemyWinning([move], this.board).length !== 0 && this.bot.checkIfMoveWillResultInPincerAttack([move], this.board).length !== 0) || this.count > 10) {
+            document.getElementById(`${move}`).click()
+          } else {
+            console.log('skipped over current random move of ' + move)
+            this.count++
+          }
+        }
+
+
+      } else {
+        move = moves[0]
+        document.getElementById(`${move}`).click()
+        this.count = 0
+      }
+
+    }
   }
 
   // Method
@@ -122,52 +183,55 @@ class Game {
 
   handleClick(evt) {
 
-    if (this.freezeBoard === true) {
-      return
-    };
+    setTimeout(() => {
+      if (this.freezeBoard === true) {
+        return
+      };
 
-    // get x from ID of clicked cell
-    var x = +evt.target.id;
+      // get x from ID of clicked cell
+      var x = +evt.target.id;
 
-    // get next spot in column (if none, ignore click)
-    var y = this.findSpotForCol(this.board, x, this.height);
-    if (y === null) {
-      return;
-    }
+      // get next spot in column (if none, ignore click)
+      var y = this.findSpotForCol(this.board, x, this.height);
+      if (y === null) {
+        return;
+      }
+
+      //update board variable with player #
+      this.board[y][x] = this.currPlayer.id
 
 
-    //update board variable with player #
-    this.board[y][x] = this.currPlayer.id
+      // place piece in board and add to HTML table
+      // TODO: add line to update in-memory board
+      this.placeInTable(y, x);
 
+      // check for tie
+      // TODO: check if all cells in board are filled; if so call, call endGame
+      let boardIsFilled = this.checkBoardIsFilled(this.board);
+      // const boardIsFilled = board.every(row => {
+      //   return row.every(cell => {
+      //     //not null means its filled. 
+      //     return cell !== null
+      //   })
+      // })
 
-    // place piece in board and add to HTML table
-    // TODO: add line to update in-memory board
-    this.placeInTable(y, x);
+      if (boardIsFilled) {
+        this.endGame('Tie Game')
+        return this.resetGame()
+      }
 
-    // check for tie
-    // TODO: check if all cells in board are filled; if so call, call endGame
-    let boardIsFilled = this.checkBoardIsFilled(this.board);
-    // const boardIsFilled = board.every(row => {
-    //   return row.every(cell => {
-    //     //not null means its filled. 
-    //     return cell !== null
-    //   })
-    // })
+      // check for win
+      if (this.checkForWin(this.board, this.currPlayer.id, this.height, this.width)) {
+        this.endGame(`Player ${this.currPlayer.id} won!`);
+        return this.resetGame()
+      }
 
-    if (boardIsFilled) {
-      this.endGame('Tie Game')
-      return this.resetGame()
-    }
+      // switch players
+      // TODO: switch currPlayer 1 <-> 2
+      this.currPlayer = this.currPlayer.id === 1 ? this.players[1] : this.players[0]
+      this.doBotMove();
+    }, 100);
 
-    // check for win
-    if (this.checkForWin(this.board, this.currPlayer.id, this.height, this.width)) {
-      this.endGame(`Player ${this.currPlayer.id} won!`);
-      return this.resetGame()
-    }
-
-    // switch players
-    // TODO: switch currPlayer 1 <-> 2
-    this.currPlayer = this.currPlayer.id === 1 ? this.players[1] : this.players[0]
   }
 
   checkBoardIsFilled(board) {
@@ -266,62 +330,62 @@ class Player {
 }
 
 
-var count = 0;
+// var count = 0;
 
-setInterval(function () {
+// setInterval(function () {
 
-  if (game === undefined) {
-    return
-  }
+//   if (game === undefined) {
+//     return
+//   }
 
-  let move = 0
-  let moves = []
-  if (game.currPlayer.id === 1) {
+//   let move = 0
+//   let moves = []
+//   if (game.currPlayer.id === 1) {
 
-    let winningMoves = findWinningMove(game.board, 1)
-    let defenseMoves = findDefenceMove(game.board, 1)
-    let futureWinMove = findFutureWinningMove(game.board, 1)
-    futureWinMove = checkIfMoveWillResultInEnemyWinning(futureWinMove, game.board)
-    futureWinMove = checkIfMoveWillResultInPincerAttack(futureWinMove, game.board)
-    let futureDefenseMove = findFutureDefenseMove(game.board, 1)
-    futureDefenseMove = checkIfMoveWillResultInEnemyWinning(futureDefenseMove, game.board)
-    futureDefenseMove = checkIfMoveWillResultInPincerAttack(futureDefenseMove, game.board)
-    let futurePincerMove = findFuturePincerMove(game.board, 1)
-    futurePincerMove = checkIfMoveWillResultInEnemyWinning(futurePincerMove, game.board)
-    let pincerDefenseMove = findPincerDefenseMove(game.board, 1)
-    pincerDefenseMove = checkIfMoveWillResultInEnemyWinning(pincerDefenseMove, game.board)
-    pincerDefenseMove = checkIfMoveWillResultInPincerAttack(pincerDefenseMove, game.board)
+//     let winningMoves = findWinningMove(game.board, 1)
+//     let defenseMoves = findDefenceMove(game.board, 1)
+//     let futureWinMove = findFutureWinningMove(game.board, 1)
+//     futureWinMove = checkIfMoveWillResultInEnemyWinning(futureWinMove, game.board)
+//     futureWinMove = checkIfMoveWillResultInPincerAttack(futureWinMove, game.board)
+//     let futureDefenseMove = findFutureDefenseMove(game.board, 1)
+//     futureDefenseMove = checkIfMoveWillResultInEnemyWinning(futureDefenseMove, game.board)
+//     futureDefenseMove = checkIfMoveWillResultInPincerAttack(futureDefenseMove, game.board)
+//     let futurePincerMove = findFuturePincerMove(game.board, 1)
+//     futurePincerMove = checkIfMoveWillResultInEnemyWinning(futurePincerMove, game.board)
+//     let pincerDefenseMove = findPincerDefenseMove(game.board, 1)
+//     pincerDefenseMove = checkIfMoveWillResultInEnemyWinning(pincerDefenseMove, game.board)
+//     pincerDefenseMove = checkIfMoveWillResultInPincerAttack(pincerDefenseMove, game.board)
 
-    moves = winningMoves.concat(defenseMoves).concat(futurePincerMove).concat(pincerDefenseMove).concat(futureWinMove).concat(futureDefenseMove)
-    console.log("winning move is " + winningMoves)
-    console.log("defense move is " + defenseMoves)
-    console.log("pincer move is " + futurePincerMove)
-    console.log("pincer defence move is " + pincerDefenseMove)
-    console.log("future winning move is " + futureWinMove)
-    console.log("future defense move is " + futureDefenseMove)
-    console.log('---------------------')
+//     moves = winningMoves.concat(defenseMoves).concat(futurePincerMove).concat(pincerDefenseMove).concat(futureWinMove).concat(futureDefenseMove)
+//     console.log("winning move is " + winningMoves)
+//     console.log("defense move is " + defenseMoves)
+//     console.log("pincer move is " + futurePincerMove)
+//     console.log("pincer defence move is " + pincerDefenseMove)
+//     console.log("future winning move is " + futureWinMove)
+//     console.log("future defense move is " + futureDefenseMove)
+//     console.log('---------------------')
 
-    if (moves.length === 0) {
-      move = Math.floor(Math.random() * (game.width - 1))
-      var y = findSpotForCol(game.board, move, game.board.length);
-      if (y !== null) {
+//     if (moves.length === 0) {
+//       move = Math.floor(Math.random() * (game.width - 1))
+//       var y = findSpotForCol(game.board, move, game.board.length);
+//       if (y !== null) {
 
-        if ((checkIfMoveWillResultInEnemyWinning([move], game.board).length !== 0 && checkIfMoveWillResultInPincerAttack([move], game.board).length !== 0) || count > 10) {
-          document.getElementById(`${move}`).click()
-        } else {
-          console.log('skipped over current random move of ' + move)
-          count++
-        }
-      }
-
-
-    } else {
-      move = moves[0]
-      document.getElementById(`${move}`).click()
-      count = 0
-    }
+//         if ((checkIfMoveWillResultInEnemyWinning([move], game.board).length !== 0 && checkIfMoveWillResultInPincerAttack([move], game.board).length !== 0) || count > 10) {
+//           document.getElementById(`${move}`).click()
+//         } else {
+//           console.log('skipped over current random move of ' + move)
+//           count++
+//         }
+//       }
 
 
-  }
+//     } else {
+//       move = moves[0]
+//       document.getElementById(`${move}`).click()
+//       count = 0
+//     }
 
-}, 150);
+
+//   }
+
+// }, 150);
